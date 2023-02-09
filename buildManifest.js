6877fe1e8e97d37696fba98bce8cffe84d5b1cb9
Vitaly Tikhoplav <tikhoplavspb@gmail.com>
2023-02-09 03:11:47 +0200

    fixed underscore problem

--- buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["868-639c2ad53af1a005.js","index-283e6cfbb05070ff.js"],sortedPages:["/","/_app"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
