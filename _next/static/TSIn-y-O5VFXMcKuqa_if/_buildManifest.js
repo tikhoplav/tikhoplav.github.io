e07aed20b9f5074e1f598f6565291096aab3d607
Vitaly Tikhoplav <tikhoplavspb@gmail.com>
2023-02-09 02:54:14 +0200

    flattened struct

--- _next/static/TSIn-y-O5VFXMcKuqa_if/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/868-639c2ad53af1a005.js","static/chunks/pages/index-283e6cfbb05070ff.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
