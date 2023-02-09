5bc719d4f16ebd41b89aa458361fbd2e7a94c20d
Vitaly Tikhoplav <tikhoplavspb@gmail.com>
2023-02-09 02:22:40 +0200

    added content

---
+++ _next/static/VrATQo2CqOJwQ3RLI0Bhq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/868-639c2ad53af1a005.js","static/chunks/pages/index-283e6cfbb05070ff.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
